eab16410d086aeebd5ef3b91d40840362664590a
veronica <sandyff321@gmail.com>
2025-07-21 16:20:18 -0700

    Remove basename from BrowserRouter for Vercel and add vercel.json for SPA routing support

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/bcostrichfarm">
+      <BrowserRouter>
         <div className="min-h-screen flex flex-col">
           <Navigation />
         <Routes>
